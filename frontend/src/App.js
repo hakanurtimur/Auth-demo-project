38196f43d8fffa2094b3580e5371fe2a92d46132
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-04-08 21:20:17 +0300

    Adding user logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as authAction } from './pages/Authentication';
+import {action as logoutAction } from './pages/Logout'
 
 
 const router = createBrowserRouter([
@@ -24,6 +25,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       {path: 'auth', element: <AuthenticationPage />, action: authAction },
+      {path: 'logout', action: logoutAction },
       {
         path: 'events',
         element: <EventsRootLayout />,
---
+++ frontend/src/pages/Logout.js
@@ -0,0 +1,11 @@
+import { redirect } from "react-router-dom";
+
+
+export function action () {
+
+
+    localStorage.removeItem('token');
+
+    return redirect('/')
+}
+
